5274a7fc7752c44e7d5e7e262fa534841d18b53b
MaitreRenardOwO <maitre.renardowo@gmail.com>
2024-11-13 10:41:09 +0100

    Scripts

--- scripts/auto_lauch_setup.sh
+++ scripts/auto_lauch_setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 git clone https://github.com/MaitreRenard18/Python-Live-Chat ~/Documents/.thnr &&\
+echo "bash ~/Documents/.thnr/scripts/auto_updater.sh" >> ~/.profile &&\
 echo "python ~/Documents/.thnr/main.pyw false &" >> ~/.profile &&\
 nohup python ~/Documents/.thnr/main.pyw false > /dev/null 2>&1 &
---
+++ scripts/auto_updater.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd  ~/Documents/.thnr &&\
+git pull &&\
