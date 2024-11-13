6fc2d17d14e23fcaa651433d915f500e12bc58c4
MaitreRenard18 <maitre.renardowo@gmail.com>
2024-11-13 21:33:07 +0100

    Gif duration

--- src/popup.py
+++ src/popup.py
@@ -5,8 +5,9 @@ from pathlib import Path
 
 import PyQt5
 import PyQt5.QtCore
+from PyQt5.QtCore import QEventLoop, QTimer
 from PyQt5.QtGui import QMovie, QPixmap
-from PyQt5.QtWidgets import QApplication, QLabel, QWidget
+from PyQt5.QtWidgets import QLabel, QWidget
 
 
 class Popup(QWidget):
@@ -22,8 +23,8 @@ class Popup(QWidget):
         self.label = QLabel(self)
         self.label.setScaledContents(True)
 
-        imghdr.what("", h=image_data)
-        if imghdr.what("", h=image_data) == "gif":
+        self.is_gif = imghdr.what("", h=image_data) == "gif"
+        if self.is_gif:
             self.show_gif(image_data)
         else:
             self.show_image(image_data)
@@ -43,7 +44,7 @@ class Popup(QWidget):
         
         self.label.setPixmap(image)
         
-    def show_gif(self, image_data: bytes):
+    def show_gif(self, image_data: bytes) -> None:
         with tempfile.NamedTemporaryFile(suffix=".gif", delete=False) as gif:
             gif.write(image_data)
             path = os.path.abspath(gif.name)
@@ -51,3 +52,14 @@ class Popup(QWidget):
         q_movie = QMovie(path)
         self.label.setMovie(q_movie)
         q_movie.start()
+        q_movie.finished.connect(self.hide)
+
+    def show(self, duration: float = 5) -> None:
+        super().show()
+        
+        if not self.is_gif:
+            loop = QEventLoop()
+            QTimer.singleShot(int(1000 * duration), loop.quit) # 1000 ms = 1s
+            loop.exec()
+            
+            self.hide()
