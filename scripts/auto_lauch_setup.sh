476d77406ed30df4e5c78ed07c83299d0324d2db
MaitreRenard18 <maitre.renardowo@gmail.com>
2024-11-15 18:23:59 +0100

    Deleted scripts

--- scripts/auto_lauch_setup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-git clone https://github.com/MaitreRenard18/Python-Live-Chat ~/Documents/.thnr &&\
-echo "python ~/Documents/.thnr/main.pyw false &" >> ~/.profile &&\
-nohup python ~/Documents/.thnr/main.pyw false > /dev/null 2>&1 &
--- scripts/uninstall.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-sed -i 'bash ~/Documents/.thnr/scripts/auto_updater.sh &' ~/.profile &&\
-sed -i 'python ~/Documents/.thnr/main.pyw false &' ~/.profile &&\
-rm -rf ~/Documents/.thnr &&\
