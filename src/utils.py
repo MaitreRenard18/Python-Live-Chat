08caa0bda5258018c17bf28f90401aedc045acf0
LucasTHOMAS18 <maitre.renardowo@gmail.com>
2024-11-19 16:17:30 +0100

    Path fix

--- src/utils.py
+++ src/utils.py
@@ -6,4 +6,4 @@ def resource_path(relative_path):
         if hasattr(sys, '_MEIPASS'):
             return os.path.join(sys._MEIPASS, relative_path)
         
-        return os.path.join(os.path.abspath("."), relative_path)
+        return os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", relative_path)
